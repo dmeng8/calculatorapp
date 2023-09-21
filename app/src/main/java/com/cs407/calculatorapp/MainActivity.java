47e0ea3706e7103c35dbd35114491dc7a7b93888
orion <dmeng8@wisc.edu>
2023-09-21 00:18:02 -0500

    created basic calculator operations

--- app/src/main/java/com/cs407/calculatorapp/MainActivity.java
+++ app/src/main/java/com/cs407/calculatorapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.cs407.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickAdd(View view) {
+        EditText myTextField1=(EditText) findViewById(R.id.myTextField1);
+        EditText myTextField2=(EditText) findViewById(R.id.myTextField2);
+        if(myTextField1.getText().toString().isEmpty() || myTextField2.getText().toString().isEmpty()) {
+            goToError("ERROR: missing input(s)");
+        } else {
+            double num1 = Double.parseDouble(myTextField1.getText().toString());
+            double num2 = Double.parseDouble(myTextField2.getText().toString());
+            goToSolution(num1+num2);
+        }
+    }
+
+    public void clickSubtract(View view) {
+        EditText myTextField1=(EditText) findViewById(R.id.myTextField1);
+        EditText myTextField2=(EditText) findViewById(R.id.myTextField2);
+        if(myTextField1.getText().toString().isEmpty() || myTextField2.getText().toString().isEmpty()) {
+            goToError("ERROR: missing input(s)");
+        } else {
+            double num1 = Double.parseDouble(myTextField1.getText().toString());
+            double num2 = Double.parseDouble(myTextField2.getText().toString());
+            goToSolution(num1-num2);
+        }
+    }
+
+    public void clickMultiply(View view) {
+        EditText myTextField1=(EditText) findViewById(R.id.myTextField1);
+        EditText myTextField2=(EditText) findViewById(R.id.myTextField2);
+        if(myTextField1.getText().toString().isEmpty() || myTextField2.getText().toString().isEmpty()) {
+            goToError("ERROR: missing input(s)");
+        } else {
+            double num1 = Double.parseDouble(myTextField1.getText().toString());
+            double num2 = Double.parseDouble(myTextField2.getText().toString());
+            goToSolution(num1*num2);
+        }
+    }
+
+    public void clickDivide(View view) {
+        EditText myTextField1=(EditText) findViewById(R.id.myTextField1);
+        EditText myTextField2=(EditText) findViewById(R.id.myTextField2);
+        if(myTextField1.getText().toString().isEmpty() || myTextField2.getText().toString().isEmpty()) {
+            goToError("ERROR: missing input(s)");
+        } else {
+            double num1 = Double.parseDouble(myTextField1.getText().toString());
+            double num2 = Double.parseDouble(myTextField2.getText().toString());
+            if(num2==0) goToError("ERROR: divide by zero");
+            else goToSolution(num1/num2);
+        }
+    }
+
+    public void goToSolution(double d) {
+        Intent intent=new Intent(this, SolutionActivity.class);
+        intent.putExtra("message", Double.toString(d));
+        startActivity(intent);
+    }
+
+    public void goToError(String s) {
+        Intent intent=new Intent(this, SolutionActivity.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
     }
 }
